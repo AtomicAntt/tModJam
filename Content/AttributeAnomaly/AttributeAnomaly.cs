89443ead32c628ed75e09b2db45da0a929c03f8f
AtomicAntt <anthonychi36@gmail.com>
2025-07-12 12:39:56 -0700

    Also find out if a buff is re-applied to an enemy

--- Content/AttributeAnomaly/AttributeAnomaly.cs
+++ Content/AttributeAnomaly/AttributeAnomaly.cs
@@ -10,6 +10,7 @@ namespace ZZZMod.Content.AttributeAnomaly
     {
         public override bool InstancePerEntity => true;
         public int[] previousBuffs = new int[20]; // I know that max buffs is 20
+        public int[] previousBuffTimes = new int[20];
 
         public override void AI(NPC npc)
         {
@@ -19,10 +20,16 @@ namespace ZZZMod.Content.AttributeAnomaly
                 {
                     Main.NewText($"{npc.FullName} just got this buff: {Lang.GetBuffName(npc.buffType[i])}");
                 }
+
+                if (npc.buffType[i] != 0 && previousBuffs[i] != 0 && previousBuffTimes[i] < npc.buffTime[i]) // If the debuff time increases, we can assume it is reapplied.
+                {
+                    Main.NewText($"{npc.FullName} is hit with this buff again: {Lang.GetBuffName(npc.buffType[i])}");
+                }
             }
      
-            // Update previous buffs
+            // Update previous buffs & times
             npc.buffType.CopyTo(previousBuffs, 0);
+            npc.buffTime.CopyTo(previousBuffTimes, 0);
         }
     }
 }
